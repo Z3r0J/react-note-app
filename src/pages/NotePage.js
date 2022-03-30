3ffa7411481ac223a9bc1ef5046d2e968ec9c916
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-03-30 15:44:02 -0400

    Add local API based in json-server

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, { useState, useEffect } from 'react'
 import { ReactComponent as ArrowLeft } from '../assets/chevron-left.svg'
 import { Link } from 'react-router-dom';
+import { __esModule } from 'react-router-dom/cjs/react-router-dom.min';
  
 function NotePage({match}) {
     let noteId = match.params.id;
-    let note = notes.find(note => note.id == noteId);
+    let [note, setNote] = useState(null);
+
+    useEffect(()=>{
+        let noteId = match.params.id;
+        getNotes();
+    },[noteId]);
+
+    let getNotes = async() =>{
+    let response = await fetch(`http://127.0.0.1:5000/notes/${noteId}`);
+    let data = await response.json();
+        setNote(data);
+    };
+
   return (
     <div className="note">
         <div className="note-header">
